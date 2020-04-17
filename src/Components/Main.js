3caa7f201b8059b5af64514e20db65a386fa3686
kishansinghchauhan <kishancoer7071@gmail.com>
2020-04-17 19:57:16 +0530

    Pass data on components

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -11,6 +11,9 @@ import Profile from './Profile';
 
 import Footer from './Footer';
 import { INTERNS } from '../Shared/Interns';
+import { PROJECTS } from '../Shared/Projects';
+import { SKILLS } from '../Shared/Skills';
+import { EDUCATION } from '../Shared/Education';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -19,6 +22,9 @@ class Main extends Component {
         super(props);
         this.state = {
           interns:INTERNS,
+          projects:PROJECTS,
+          skills:SKILLS,
+          education:EDUCATION
         };
     }
     render() { 
@@ -31,9 +37,9 @@ class Main extends Component {
                     <Route exact path='/Mentor' component={Mentor} />
                     <Route exact path='/Experience' component={Experience} />
                     <Route exact path='/Contact' component={Contact} />
-                    <Route exact path='/Projects' component={Projects} />
-                    <Route exact path='/Skills' component={Skills} />
-                    <Route exact path='/Education' component={Education} />
+                    <Route exact path='/Projects' component={() => <Projects  projects = { this.state.projects }/> } />
+                    <Route exact path='/Skills' component={() => <Skills  skills = { this.state.skills }/> } />
+                    <Route exact path='/Education' component={() => <Education  education = { this.state.education }/> } />
                     <Route exact path='/Profile' component={Profile} />
                     <Redirect to="/About" />
                 </Switch>
