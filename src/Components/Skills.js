9aa1c35b681eb20cdf35cd57ebc2d4dedf3af2b9
harshjoshitriceria <harshjoshi.triceria@gmail.com>
2020-04-20 19:28:26 +0530

    skillsanimation

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,11 +1,20 @@
 import React,{Component} from 'react'
 import '../Styles/Skills.css'
 import { Progress } from 'reactstrap'
-
+import { TransitionGroup } from 'react-transition-group';
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 
 class Skills extends Component{
 
+componentDidMount(){
+  AOS.init({duration:1500});
+}
+
+
     render(){
+
+        
         const skills = this.props.skills.filter((intern) => intern.internid === 0).map((skill) => {
             return(
                 <div className="col-6 text-center mb-5">
@@ -13,10 +22,10 @@ class Skills extends Component{
                         <h3>{skill.name}</h3>
                         <h5>{skill.rating} <span>%</span></h5>
                     </div>
-                     <div style={{ marginLeft:"15%" }}>
-                        <Progress  color="success" value= { skill.rating } />
-                        
-                    </div>
+                         
+                <div style={{ marginLeft:"15%" }} class="progress" >
+  <div data-aos='fade-right' data-aos-easing="ease-in-sine" class="progress-bar bg-success" role="progressbar" style={{width:skill.rating ? `${skill.rating}%`:"0%"}}  aria-valuemin="0" aria-valuemax="100"></div>
+</div>
                 </div>
             )
         })
@@ -24,7 +33,10 @@ class Skills extends Component{
            <div className="container mt-5">
             <h2 className="text-center text-warning">Skills</h2>
             <div className="row">
-                {skills}
+               
+{skills}
+
+                
                 {/* <div className="col-md-4 col-6 text-center">
                     <h3>HTML5</h3>
                     <span>8</span>
@@ -44,6 +56,7 @@ class Skills extends Component{
                 <div className="col-md-4 col-6 text-center">
                     <h3>SEO</h3>
                 </div> */}
+            
             </div>
             </div>
         )
