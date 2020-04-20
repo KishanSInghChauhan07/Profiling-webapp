68a1451f2d61f2173fda52f24348204e846d67b9
kishansinghchauhan <kishancoer7071@gmail.com>
2020-04-20 22:30:14 +0530

    ++

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -8,7 +8,7 @@ import 'aos/dist/aos.css';
 class Skills extends Component{
 
 componentDidMount(){
-  AOS.init({duration:1500});
+  AOS.init({ duration:1500 });
 }
 
 
@@ -19,45 +19,21 @@ componentDidMount(){
             return(
                 <div className="col-6 text-center mb-5">
                     <div className="d-flex justify-content-around">
-                        <h3>{skill.name}</h3>
+                        <h3>{ skill.name }</h3>
                         <h5>{skill.rating} <span>%</span></h5>
                     </div>
-                         
-                <div style={{ marginLeft:"15%" }} class="progress" >
-  <div data-aos='fade-right' data-aos-easing="ease-in-sine" class="progress-bar bg-success" role="progressbar" style={{width:skill.rating ? `${skill.rating}%`:"0%"}}  aria-valuemin="0" aria-valuemax="100"></div>
-</div>
+                    <div style={{ marginLeft:"15%" }} class="progress" >
+                        <div data-aos='fade-right' data-aos-easing="ease-in-sine" class="progress-bar bg-success" role="progressbar" style={{width:skill.rating ? `${skill.rating}%`:"0%"}}  aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
                 </div>
             )
         })
         return (
-           <div className="container mt-5">
-            <h2 className="text-center text-warning">Skills</h2>
-            <div className="row">
-               
-{skills}
-
-                
-                {/* <div className="col-md-4 col-6 text-center">
-                    <h3>HTML5</h3>
-                    <span>8</span>
+            <div className="container mt-5">
+                <h2 className="text-center text-warning">Skills</h2>
+                <div className="row">
+                    { skills }
                 </div>
-                <div className="col-md-4 col-6 text-center">
-                    <h3>CSS3</h3>
-                </div>
-                <div className="col-md-4 col-6 text-center">
-                    <h3>Bootstrap</h3>
-                </div>
-                <div className="col-md-4 col-6 text-center">
-                    <h3>JavaScript</h3>
-                </div>
-                <div className="col-md-4 col-6 text-center">
-                    <h3>Web Design</h3>
-                </div>
-                <div className="col-md-4 col-6 text-center">
-                    <h3>SEO</h3>
-                </div> */}
-            
-            </div>
             </div>
         )
     }
